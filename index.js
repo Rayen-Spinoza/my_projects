c58c091be5dc70fccf49c12217bd1f7be42388b6
Rayen Spinoza <elnoza0@gmail.com>
2024-11-08 11:47:24 +0100

    Committed

---
+++ index.js
@@ -0,0 +1,11 @@
+const pi = 3.14; //const variable darf nicht verändert werden
+var x= 5;
+let num= 100;
+let decimal= 52.22;
+let bool =true;
+let name= "MyName"
+let website = {type: blog, status: 404} //let object= {property : value}
+function myFunction (a,b) {
+    return a*b ;
+}
+
